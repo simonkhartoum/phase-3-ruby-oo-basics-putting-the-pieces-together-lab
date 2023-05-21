4f5446836e9bf60a9c376b9d60564de6f0aa42e1
simon <simonkhartoum@gmail.com>
2023-05-21 21:31:12 +0300

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+  attr_accessor :title, :page_count, :genre, :author
 
+  def initialize(title="And Then There Were None")
+      @title = title
+  end
 
+  def turn_page
+      puts "Flipping the page...wow, you read fast!"
+  end
+end
